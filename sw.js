44fccbf86975d56f33849fe138cf06ff4cf355cc
hafa786 <hafizm.shehzad@gmail.com>
2019-02-16 01:47:39 +0200

    wip for service worker

---
+++ sw.js
@@ -0,0 +1,30 @@
+// self.addEventListener("install", event => {
+//     event.waitUntil(caches.open(precache-v1)
+//     .then(cache => { cache.addAll(["/","app.js"]);
+// })
+// );
+// });
+
+// self.addEventListener("fetch", event => {
+//     event.respondWith(
+//       caches.match(event.request).then(response => {
+//         return response || fetch(event.request);
+//       })
+//     );
+//   });
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open("precache-v1").then(Cache => {
+        Cache.addAll(["/", "app.js"]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
